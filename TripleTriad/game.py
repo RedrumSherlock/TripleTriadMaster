aacd1bc58f3ffcf774b582eeccff8bccda2292bc
mwang <wml816@gmail.com>
2017-09-04 22:20:09 -0600

    Initial Creation

---
+++ TripleTriad/game.py
@@ -0,0 +1,17 @@
+'''
+Created on Aug 30, 2017
+
+@author: Mengliao Wang
+'''
+
+class GameState(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
